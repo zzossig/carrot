093a819ffb403007b0eb00bb2a04e7488809e204
zzossig <zzossig@gmail.com>
2021-04-06 01:49:11 +0900

    ast String method added, ast Expression interface added

--- ast/ast.go
+++ ast/ast.go
@@ -1,10 +1,33 @@
 package ast
 
-import "github.com/zzossig/carrot/token"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/zzossig/carrot/token"
+)
+
+// Expression is a type maker
+type Expression interface {
+	expression()
+	String() string
+}
 
 // Group ::= selector [ COMMA S* selector ]*
 type Group struct {
 	Selectors []Selector
+}
+
+func (g *Group) expression() {}
+func (g *Group) String() string {
+	var sb strings.Builder
+	for i, s := range g.Selectors {
+		sb.WriteString(s.String())
+		if i < len(g.Selectors)-1 {
+			sb.WriteString(", ")
+		}
+	}
+	return sb.String()
 }
 
 // Selector ::= simple_selector_sequence [ combinator simple_selector_sequence ]*
@@ -14,13 +37,52 @@ type Selector struct {
 	Token token.Token
 }
 
+func (s *Selector) expression() {}
+func (s *Selector) String() string {
+	var sb strings.Builder
+
+	sb.WriteString(s.Left.String())
+	switch s.Token.Type {
+	case token.PLUS:
+		fallthrough
+	case token.GT:
+		fallthrough
+	case token.TILDE:
+		sb.WriteString(s.Token.Literal)
+	default:
+		sb.WriteString(" ")
+	}
+	sb.WriteString(s.Right.String())
+
+	return sb.String()
+}
+
 // Sequence ::= [ type_selector | universal ]
 //     		 simple_sequence*
 //   		 | simple_sequence+
 type Sequence struct {
 	TypeSelector
 	Universal
+	Seq    []SimpleSelector
 	TypeID byte
+}
+
+func (s *Sequence) expression() {}
+func (s *Sequence) String() string {
+	var sb strings.Builder
+
+	switch s.TypeID {
+	case 1:
+		sb.WriteString(s.TypeSelector.String())
+	case 2:
+		sb.WriteString(s.Universal.String())
+	}
+
+	for _, ss := range s.Seq {
+		sb.WriteString(ss.String())
+	}
+
+	return sb.String()
 }
 
 // SimpleSelector ::= [ HASH | class | attrib | pseudo | negation ]
@@ -33,9 +95,31 @@ type SimpleSelector struct {
 	TypeID byte
 }
 
+func (ss *SimpleSelector) expression() {}
+func (ss *SimpleSelector) String() string {
+	switch ss.TypeID {
+	case 1:
+		return ss.Hash.String()
+	case 2:
+		return ss.Class.String()
+	case 3:
+		return ss.Attrib.String()
+	case 4:
+		return ss.Pseudo.String()
+	case 5:
+		return ss.Negation.String()
+	}
+	return ""
+}
+
 // TypeSelector ::= element_name
 type TypeSelector struct {
 	ElementName
+}
+
+func (ts *TypeSelector) expression() {}
+func (ts *TypeSelector) String() string {
+	return ts.ElementName.String()
 }
 
 // element_name ::= IDENT
@@ -43,9 +127,19 @@ type ElementName struct {
 	Ident
 }
 
+func (en *ElementName) expression() {}
+func (en *ElementName) String() string {
+	return en.Ident.String()
+}
+
 // Universal ::= '*'
 type Universal struct {
 	Token token.Token
+}
+
+func (u *Universal) expression() {}
+func (u *Universal) String() string {
+	return u.Token.Literal
 }
 
 // Class ::= '.' IDENT
@@ -53,9 +147,19 @@ type Class struct {
 	Ident
 }
 
+func (c *Class) expression() {}
+func (c *Class) String() string {
+	return fmt.Sprintf(".%s", c.Ident.String())
+}
+
 // Hash ::= '#' Ident
 type Hash struct {
 	Ident
+}
+
+func (h *Hash) expression() {}
+func (h *Hash) String() string {
+	return fmt.Sprintf("#%s", h.Ident.String())
 }
 
 // Attrib ::= '[' S* [ namespace_prefix ]? IDENT S*
@@ -72,6 +176,17 @@ type Attrib struct {
 	TypeID      byte
 }
 
+func (a *Attrib) expression() {}
+func (a *Attrib) String() string {
+	switch a.TypeID {
+	case 1:
+		return fmt.Sprintf("[%s]", a.Left.String())
+	case 2:
+		return fmt.Sprintf("[%s%s%s]", a.Left.Value, a.Token.Literal, a.Right.Value)
+	}
+	return ""
+}
+
 // Negation ::= NOT S* negation_arg S* ')'
 // negation_arg ::= type_selector | universal | HASH | class | attrib | pseudo
 type Negation struct {
@@ -84,6 +199,28 @@ type Negation struct {
 	TypeID byte
 }
 
+func (n *Negation) expression() {}
+func (n *Negation) String() string {
+	var sb strings.Builder
+	sb.WriteString(":not(")
+	switch n.TypeID {
+	case 1:
+		sb.WriteString(n.TypeSelector.String())
+	case 2:
+		sb.WriteString(n.Universal.String())
+	case 3:
+		sb.WriteString(n.Hash.String())
+	case 4:
+		sb.WriteString(n.Class.String())
+	case 5:
+		sb.WriteString(n.Attrib.String())
+	case 6:
+		sb.WriteString(n.Pseudo.String())
+	}
+	sb.WriteString(")")
+	return sb.String()
+}
+
 // Pseudo ::= ':' ':'? [ IDENT | functional_pseudo ]
 type Pseudo struct {
 	Ident
@@ -92,18 +229,52 @@ type Pseudo struct {
 	TypeID byte
 }
 
-// FunctionalPseudo
-type FunctionalPseudo struct {
-	Expression
+func (p *Pseudo) expression() {}
+func (p *Pseudo) String() string {
+	var sb strings.Builder
+	sb.WriteString(p.Token.Literal)
+	switch p.TypeID {
+	case 1:
+		sb.WriteString(p.Ident.String())
+	case 2:
+		sb.WriteString(p.FunctionalPseudo.String())
+	}
+	return sb.String()
 }
 
-// Expression ::= DIMENSION | NUMBER | STRING | IDENT
-type Expression struct {
+// FunctionalPseudo ::= FUNCTION S* expression ')'
+type FunctionalPseudo struct {
+	Token token.Token
+	Arg
+}
+
+func (fp *FunctionalPseudo) expression() {}
+func (fp *FunctionalPseudo) String() string {
+	return fmt.Sprintf("%s(%s)", fp.Token.Literal, fp.Arg.String())
+}
+
+// Arg ::= DIMENSION | NUMBER | STRING | IDENT
+type Arg struct {
 	Ident
-	String
+	Str
 	Number
 	Dimension
 	TypeID byte
+}
+
+func (a *Arg) String() string {
+	switch a.TypeID {
+	case 1:
+		return a.Dimension.String()
+	case 2:
+		return a.Number.String()
+	case 3:
+		return a.Str.String()
+	case 4:
+		return a.Ident.String()
+	default:
+		return ""
+	}
 }
 
 // Number ::= int
@@ -111,17 +282,33 @@ type Number struct {
 	Value int
 }
 
+func (n *Number) String() string {
+	return fmt.Sprintf("%d", n.Value)
+}
+
 // Ident ::= string
 type Ident struct {
 	Value string
 }
 
-// String ::= string
-type String struct {
+func (i *Ident) String() string {
+	return i.Value
+}
+
+// Str ::= string
+type Str struct {
 	Value string
+}
+
+func (s *Str) String() string {
+	return s.Value
 }
 
 // Dimension ::= an + b
 type Dimension struct {
 	A, B int
+}
+
+func (d *Dimension) String() string {
+	return fmt.Sprintf("%dn + %d", d.A, d.B)
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,183 @@
 package parser
+
+import (
+	"fmt"
+
+	"github.com/zzossig/carrot/ast"
+	"github.com/zzossig/carrot/lexer"
+	"github.com/zzossig/carrot/token"
+)
+
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
+
+type Parser struct {
+	l      *lexer.Lexer
+	errors []string
+
+	curToken  token.Token
+	peekToken token.Token
+
+	prefixParseFns map[token.Type]prefixParseFn
+	infixParseFns  map[token.Type]infixParseFn
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
+
+	p.prefixParseFns = make(map[token.Type]prefixParseFn)
+	p.prefixParseFns[token.STRING] = p.parseString
+	p.prefixParseFns[token.IDENT] = p.parseIdent
+	p.prefixParseFns[token.NUM] = p.parseNumber
+	p.prefixParseFns[token.HASH] = p.parseHash
+	p.prefixParseFns[token.DOT] = p.parseClass
+	p.prefixParseFns[token.COLON] = p.parsePseudo
+	p.prefixParseFns[token.DCOLON] = p.parsePseudo
+	p.prefixParseFns[token.ASTERISK] = p.parseUniversal
+
+	p.infixParseFns = make(map[token.Type]infixParseFn)
+	p.infixParseFns[token.PLUS] = p.parsePlus
+	p.infixParseFns[token.GT] = p.parseGreater
+	p.infixParseFns[token.EQ] = p.parseEqual
+	p.infixParseFns[token.TILDE] = p.parseTilde
+	p.infixParseFns[token.INCLUDES] = p.parseIncludes
+	p.infixParseFns[token.DASHMATCH] = p.parseDashMatch
+	p.infixParseFns[token.PREFIXMATCH] = p.parsePrefixMatch
+	p.infixParseFns[token.SUFFIXMATCH] = p.parseSuffixMatch
+	p.infixParseFns[token.SUBSTRINGMATCH] = p.parseSubstringMatch
+
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) curTokenIs(t token.Type) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.Type) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.Type) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.Type) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) noPrefixParseFnError(t token.Type) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) parseExpression() ast.Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
+		return nil
+	}
+	leftExp := prefix()
+
+	for {
+		infix := p.infixParseFns[p.peekToken.Type]
+		if infix == nil {
+			return leftExp
+		}
+
+		p.nextToken()
+
+		leftExp = infix(leftExp)
+	}
+
+	return leftExp
+}
+
+func (p *Parser) parseString() ast.Expression {
+	return nil
+}
+
+func (p *Parser) parseIdent() ast.Expression {
+	return nil
+}
+
+func (p *Parser) parseNumber() ast.Expression {
+	return nil
+}
+
+func (p *Parser) parseHash() ast.Expression {
+	return nil
+}
+
+func (p *Parser) parseClass() ast.Expression {
+	return nil
+}
+
+func (p *Parser) parsePseudo() ast.Expression {
+	return nil
+}
+
+func (p *Parser) parseUniversal() ast.Expression {
+	return nil
+}
+
+func (p *Parser) parsePlus(left ast.Expression) ast.Expression {
+	return nil
+}
+
+func (p *Parser) parseGreater(left ast.Expression) ast.Expression {
+	return nil
+}
+
+func (p *Parser) parseEqual(left ast.Expression) ast.Expression {
+	return nil
+}
+
+func (p *Parser) parseTilde(left ast.Expression) ast.Expression {
+	return nil
+}
+
+func (p *Parser) parseIncludes(left ast.Expression) ast.Expression {
+	return nil
+}
+
+func (p *Parser) parseDashMatch(left ast.Expression) ast.Expression {
+	return nil
+}
+
+func (p *Parser) parsePrefixMatch(left ast.Expression) ast.Expression {
+	return nil
+}
+
+func (p *Parser) parseSuffixMatch(left ast.Expression) ast.Expression {
+	return nil
+}
+
+func (p *Parser) parseSubstringMatch(left ast.Expression) ast.Expression {
+	return nil
+}
